bad85985e9603db97c1f9bf11d42b75a056e8d12
Luke Davis <lukeda117@gmail.com>
2024-06-22 12:05:23 +0100

    Updated player movement to use AddForce

--- Assets/CFMovement.cs
+++ Assets/CFMovement.cs
@@ -7,6 +7,7 @@ public class CFMovement : MonoBehaviour
     public Rigidbody myRigidbody;
     public float speed;
     private Vector3 transformation;
+    private Quaternion rotation;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +20,13 @@ public class CFMovement : MonoBehaviour
     {
         // Get vector to transform player by
         transformation = new Vector3(Input.GetAxis("Horizontal"), 0 , Input.GetAxis("Vertical"));
+        // Get rotation vector to rotate player by
     }
 
     // FixedUpdate is called whenever is needed per frame
     void FixedUpdate()
     {
         // Movement using Input.Getaxis
-        myRigidbody.velocity += transformation * speed * Time.deltaTime;
+        myRigidbody.AddForce(transformation * speed);
     }
 }
