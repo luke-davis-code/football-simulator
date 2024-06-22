fc2eaf77159588e80aa0fd89b6173bd1493d5e32
Luke Davis <lukeda117@gmail.com>
2024-06-22 11:50:39 +0100

    Initial dribbling script added

---
+++ Assets/Dribbling.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dribbling : MonoBehaviour
+{
+    private Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Need the ball to follow the player after they collide
+            // Cannot set the player as parent as this does not work in conjuction with the ball having its own physics
+            // So add a joint between the two objects
+            FixedJoint fixedJoint = gameObject.AddComponent<FixedJoint>();
+            fixedJoint.connectedBody = collision.rigidbody;
+            // Stop collision between objects after they are connected
+            // Stops objects from continuing to collide and creating more joints
+            fixedJoint.enableCollision = false;
+        }
+    }
+}
