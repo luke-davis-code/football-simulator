dc52ed5380de9f01a40fed66a4ed4b76f45c16aa
Luke Davis <lukeda117@gmail.com>
2024-09-11 23:37:39 +0100

    Player can shoot and ball teleports to goal

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,15 +6,21 @@ public class CFMovement : MonoBehaviour
 {
     public CharacterController myController;
     public float speed;
+    public float ballSpeed;
     public float rotationSpeed;
+    private string team;
+    private string opponent;
     private Vector3 transformation;
     private Vector3 eulerAngleVelocity;
     private bool hasBall;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         hasBall = false;
+        team = "Home";
+        opponent = "Away";
     }
 
     // Update is called once per frame
@@ -53,6 +59,15 @@ public class CFMovement : MonoBehaviour
 
     private void Shoot()
     {
-        Debug.Log("Shooting");
+        // Ball should always be child 1
+        GameObject ball = this.transform.Find("Ball").gameObject;
+        GameObject goal = GameObject.Find(opponent + "/Goal");
+        // Remove ball as child of player
+        ball.transform.parent = null;
+        // Set dribbling flag to false
+        hasBall = false;
+        // Ball moves towards opponents goal
+        // MAYBE MOVE THIS INTO UPDATE AS MOVEMENT IS INSTANTANEOUS AT MOMENT
+        ball.transform.position = Vector3.MoveTowards(ball.transform.position, goal.transform.position, ballSpeed);
     }
 }
