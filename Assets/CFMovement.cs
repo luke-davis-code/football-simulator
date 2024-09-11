630f26bf602f0c87d2cedf50511f3831413ac054
Luke Davis <lukeda117@gmail.com>
2024-09-11 22:58:26 +0100

    Changed scripts so all logic is handled in player game object script

--- Assets/CFMovement.cs
+++ Assets/CFMovement.cs
@@ -9,11 +9,12 @@ public class CFMovement : MonoBehaviour
     public float rotationSpeed;
     private Vector3 transformation;
     private Vector3 eulerAngleVelocity;
+    private bool hasBall;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasBall = false;
     }
 
     // Update is called once per frame
@@ -30,5 +31,28 @@ public class CFMovement : MonoBehaviour
             transform.rotation = Quaternion.LookRotation(transformation);
         }
         myController.Move(transformation * speed * Time.deltaTime);
+        if (hasBall)
+        {
+            if (Input.GetButtonDown("Shoot"))
+            {
+                Shoot();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider objectHit)
+    {
+        if (objectHit.gameObject.tag == "Ball")
+        {
+            // Set balls transfrom parent to this player transform
+            objectHit.transform.parent = this.transform;
+            // Set dribbling flag to true
+            hasBall = true;
+        }
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("Shooting");
     }
 }
